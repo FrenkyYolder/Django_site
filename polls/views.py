e80c74dcf1a030a2a0145cd4542e1203c97d3880
FrenkyYolder <qgc89092850880@gmail.com>
2022-07-10 09:20:38 +0300

    add views a main_page

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+# views.py
+from django.http import HttpResponse
 
-# Create your views here.
+
+def main_page(request):
+    print('Юзер на сайте')
+    return HttpResponse('Привет!')

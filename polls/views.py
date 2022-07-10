f1073b937e5e58c5dd134a13f5efe694c208d804
FrenkyYolder <qgc89092850880@gmail.com>
2022-07-10 22:31:35 +0300

    commit

--- polls/views.py
+++ polls/views.py
@@ -5,7 +5,15 @@ from django.template import loader
 
 def main_page(request):
     print('Юзер на сайте')
-    html_page = loader.get_template('main_page_1.html')
+    templates = loader.get_template('main_page_1.html')
     context = {}
-    rendered_page = html_page.render(context, request)
+    rendered_page = templates.render(context, request)
+    return HttpResponse(rendered_page)
+
+
+def second_page(request):
+    print('Юзер на второй странице')
+    templates = loader.get_template('second_page.html')
+    context = {}
+    rendered_page = templates.render(context, request)
     return HttpResponse(rendered_page)
--- pythonProject1/urls.py
+++ pythonProject1/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from polls import views
 
 urlpatterns = [
-    path('', views.main_page)
+    path('', views.main_page),
+    path('second_page/', views.second_page)
 ]

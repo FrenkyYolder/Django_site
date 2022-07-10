d63473649ba969fe482c94b8382e1c0e4ad3c4c5
FrenkyYolder <qgc89092850880@gmail.com>
2022-07-10 09:37:53 +0300

    commit

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,11 @@
 # views.py
 from django.http import HttpResponse
+from django.template import loader
 
 
 def main_page(request):
     print('Юзер на сайте')
-    return HttpResponse('Привет!')
+    html_page = loader.get_template('main_page_1.html')
+    context = {}
+    rendered_page = html_page.render(context, request)
+    return HttpResponse(rendered_page)
